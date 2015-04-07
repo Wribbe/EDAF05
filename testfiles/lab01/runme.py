89587f62dff87ae86f92d28a574667552ab4ac62
Stefan Eng <eng_steff@hotmail.com>
2015-04-07 16:52:59 +0200

    Get and structure in and out files.

--- testfiles/lab01/runme.py
+++ testfiles/lab01/runme.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python2.7
 
-print "HELLO!"
+import sys
+import os
+
+def get_files():
+    files = os.listdir(".")
+    name_dict = {}
+    for filestring in files:
+        if "swp" in filestring:
+            continue
+        filename = filestring.split(".")[0]
+        data_present = name_dict.get(filename)
+        if data_present:
+            data_present.append(filestring)
+        else:
+            name_dict[filename] = [filestring]
+    return name_dict
+
+def main(): 
+    print get_files()
+
+if __name__ == "__main__":
+    main()
