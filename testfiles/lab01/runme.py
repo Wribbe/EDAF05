28d4d09a10fab43e21377efc5ee56a62a5327f64
Stefan Eng <eng_steff@hotmail.com>
2015-04-07 17:01:48 +0200

    ignore runme, define compare function.

--- testfiles/lab01/runme.py
+++ testfiles/lab01/runme.py
@@ -7,7 +7,7 @@ def get_files():
     files = os.listdir(".")
     name_dict = {}
     for filestring in files:
-        if "swp" in filestring:
+        if "runme" in filestring:
             continue
         filename = filestring.split(".")[0]
         data_present = name_dict.get(filename)
@@ -17,8 +17,15 @@ def get_files():
             name_dict[filename] = [filestring]
     return name_dict
 
+def compare(lines1, lines2):
+    for line1, line2 in zip(lines1,lines2):
+        if line1 == line2:
+            continue
+        else:
+            return False
+    return True
+
 def main(): 
-    print get_files()
 
 if __name__ == "__main__":
     main()
