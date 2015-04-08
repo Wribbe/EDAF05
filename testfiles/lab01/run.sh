b0d595fd0939e06c39e0ae2c36b617c8de4db9ee
Stefan Eng <eng_steff@hotmail.com>
2015-04-08 10:45:17 +0200

    Make runscript that generates and compares.

---
+++ testfiles/lab01/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm -rf output
+mkdir output
+
+for file in ./*
+do
+  if [[ $file == *"sm"* && $file != *"5000"* && $file == *".in" && $file != *"worst"* ]]; then
+    fileTokens=(${file//./ })
+    filename=${fileTokens[0]}
+    filename=$( echo "$filename" | cut -c2- )
+    java Runme $file > output/${filename}.out
+    echo "output for $file done."
+  fi
+done
+
+echo "comparing outputs..."
+
+for file in ./output/*; do
+  generated_file=$( echo "$file" | cut -c3- )
+  given_file=$( echo "$file" | cut -c10- )
+  java Compare "$given_file" "$generated_file"
+done
