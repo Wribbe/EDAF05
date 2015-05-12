7941ebae1e1c65a7b5744f8735ce6d02a17df8cf
Stefan Eng <eng_steff@hotmail.com>
2015-05-12 12:41:18 +0200

    Store all data in cities dictionary.

--- testfiles/lab03/spanning_usa.py
+++ testfiles/lab03/spanning_usa.py
@@ -3,13 +3,17 @@ lines = [x.strip().replace('"','') for x in open("USA-highway-miles.in").readlin
 
 distances = False
 
+cities = {}
+
 for line in lines:
     if not distances:
         if "--" in line:
             distances = True
+        else:
+            cities[line] = {}
     if distances:
         start , end = line.split('--')
         tokens = end.split(" ")
         distance = tokens.pop(-1).replace("[",'').replace("]",'')
         end = " ".join(tokens)
-        print "{} <--> {} : {}".format(start, end, distance)
+        cities[start][end] = distance
