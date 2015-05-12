c5d25224e806c6ea5054199c7d99315d4774fe3a
Stefan Eng <eng_steff@hotmail.com>
2015-05-12 20:01:56 +0200

    Prepend to linked list, no sorting.

--- testfiles/lab03/spanning_usa.py
+++ testfiles/lab03/spanning_usa.py
@@ -12,33 +12,41 @@ def main():
             if "--" in line:
                 distances = True
             else:
-                cities[line] = {}
+                cities[line] = Node(line)
         if distances:
             start , end = line.split('--')
             tokens = end.split(" ")
             distance = tokens.pop(-1).replace("[",'').replace("]",'')
             end = " ".join(tokens)
-            cities[start][distance] = end
-    for city in cities:
-        print city
+            cities[start].add_connection(end, distance)
 
-    city_nodes = {}
+    san_diego_node = cities['San Diego'].connections
+    current_node = san_diego_node
+    while(True):
+        print(current_node.weight)
+        current_node = current_node.next
+        if current_node == "End":
+            break;
 
+class ListNode(object):
 
+    def __init__(self, weight="End", destination="End", next="End"):
+        self.weight = weight
+        self.destination = destination
+        self.next = next
 
 class Node(object):
 
     def __init__(self, name):
-        self.connections = {}
+        self.connections = ListNode()
         self.name = name
         self.visited = False
 
-    def add_connection(self,destination,distance):
-        try:
-            connections[distance].append(destination)
-        except:
-            new_list = connections[distance] = []
-            new_list.append(destination)
+    def add_connection(self,destination,weight):
+        current_connection = self.connections
+        new_node = ListNode(weight, destination, current_connection)
+        self.connections = new_node
+
 
 if __name__ == "__main__":
     main()
