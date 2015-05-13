0192a1fb3fa3edd61839d31faa04922a51ed6a42
Stefan Eng <atn08sen@student.lu.se>
2015-05-13 10:36:06 +0200

    Failing with total of 7 of 128 entires.

--- testfiles/lab03/spanning_usa.py
+++ testfiles/lab03/spanning_usa.py
@@ -20,13 +20,36 @@ def main():
             end = " ".join(tokens)
             cities[start].add_connection(end, distance)
 
-    san_diego_node = cities['San Diego'].connections
-    current_node = san_diego_node
-    while(True):
-        print "{} : {}".format(current_node.destination, current_node.weight)
-        current_node = current_node.next
-        if current_node == "End":
-            break;
+    num_cities = len(cities.keys())
+    print "number of cities: {}".format(num_cities)
+    added = 0
+    total_weight = 0
+    start_key = cities.keys()[0]
+    start_node = cities[start_key];
+    print "starting at city {}".format(start_key)
+    start_node.visited = True
+    added += 1
+    current_node = start_node
+    while(added != num_cities):
+        current_connection = current_node.pop()
+        if current_connection.destination == "End":
+            print "Error: Encounterd End, count was {}".format(added)
+            return
+        next_node = cities[current_connection.destination]
+        while(next_node.visited == True): 
+            current_connection = current_node.pop()
+            next_node = cities[current_connection.destination]
+
+        # the current next_node has not been visted.
+        # add the current_connection.weight to total_weight
+        # and mark the next_node as visited
+        # make next_node into current_node
+
+        total_weight += int(current_connection.weight)
+        current_node = next_node
+        current_node.visited = True
+        added += 1
+        print "added {} to spanning tree.".format(current_node.name)
 
 class ListNode(object):
 
