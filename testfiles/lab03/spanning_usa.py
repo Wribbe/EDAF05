f30e317b0d22acef2c069333b868e52dc3a4fb34
Stefan Eng <eng_steff@hotmail.com>
2015-05-17 19:41:00 +0200

    Use heapq, basic priority working.

--- testfiles/lab03/spanning_usa.py
+++ testfiles/lab03/spanning_usa.py
@@ -1,108 +1,32 @@
+import heapq
 
 def main():
 
     cities = parse_input()
-
-    start_city = cities.keys()[0]
-
-    print "starting in {}".format(start_city)
-
-    start_node = cities[start_city]
-    start_node.visited = True
-    current_path = []
-
-    total_weight = 0
-    current_node = start_node
-
-    print_list_node(start_node)
+    connections = cities[cities.keys()[0]][1]
     while(True):
-        next_connection = current_node.pop()
-        next_destination = next_connection.destination
-        if(next_destination == "End"):
+        try:
+            print heapq.heappop(connections)
+        except:
             break
-        total_weight += int(next_connection.weight)
-        current_node.visited = True
-
-        current_node = cities[next_destination]
-        current_path.append(current_node)
-#        while(current_node.visited):
-#            next_destination = current_node.pop().destination
-#            print "Already been in {}, trying {}".format(current_node.name, next_destination)
-#            if next_destination == "End":
-#                next_destination = 
-#                print "Depleted all destinations from {}, jumping to {}"
-#                break
-#            current_node = cities[next_destination]
 
 def parse_input():
 
     cities = {}
     lines = [x.strip().replace('"','') for x in open("USA-highway-miles.in").readlines()]
-    distances = False
+    parsing_distances = False
     for line in lines:
-        if not distances:
-            if "--" in line:
-                distances = True
-            else:
-                cities[line] = Node(line)
-        if distances:
-            start , end = line.split('--')
-            tokens = end.split(" ")
-            distance = tokens.pop(-1).replace("[",'').replace("]",'')
+        if '--' in line:
+            parsing_distances = True
+        if parsing_distances:
+            start, tokens = line.split('--')
+            tokens = tokens.split()
+            distance = tokens.pop(-1).replace('[','').replace(']','')
             end = " ".join(tokens)
-            cities[start].add_connection(end, distance)
-            cities[end].add_connection(start, distance)
+            heapq.heappush(cities[start][1], (int(distance), end))
+        else:
+            cities[line] = (line, [])
     return cities
-
-def print_list_node(node):
-    list_node = node.connections
-    while(list_node.next != "End"):
-        print "{} : {}".format(list_node.destination, list_node.weight)
-        list_node = list_node.next
-
-class ListNode(object):
-
-    def __init__(self, weight="End", destination="End", next="End"):
-        self.weight = weight
-        self.destination = destination
-        self.next = next
-
-class Node(object):
-
-    def __init__(self, name):
-        self.connections = ListNode()
-        self.name = name
-        self.visited = False
-
-    def add_connection(self,destination,weight):
-        current_connection = self.connections
-        if current_connection.destination == "End":
-            new_node = ListNode(weight, destination, current_connection)
-            self.connections = new_node
-        else:
-            if(int(weight) < int(self.connections.weight)):
-                new_node = ListNode(weight, destination, self.connections)
-                self.connections = new_node
-                return
-            previous_connection = self.connections
-            while(current_connection.destination != "End"):
-                current_weight = int(current_connection.weight)
-                new_weight = int(weight)
-                if (new_weight < current_weight):
-                    new_node = ListNode(weight, destination, current_connection)
-                    previous_connection.next = new_node
-                    return
-                previous_connection = current_connection
-                current_connection = current_connection.next
-            previous_connection.next = ListNode(weight, destination, current_connection)
-
-    def pop(self):
-        if self.connections.destination == "End":
-            return ListNode()
-        else:
-            temp = self.connections
-            self.connections = self.connections.next
-            return temp
 
 if __name__ == "__main__":
     main()
