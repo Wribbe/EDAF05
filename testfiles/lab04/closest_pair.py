6b67b8b13b5eb617e4004219b5a43cefcaf7843d
Stefan Eng <Stefan_Eng@users.noreply.github.com>
2015-05-18 17:09:21 +0200

    Use heapq.

--- testfiles/lab04/closest_pair.py
+++ testfiles/lab04/closest_pair.py
@@ -1,6 +1,22 @@
-lines = [" ".join(x.strip().split()[1:])
-         for x in open("files/rl11849.tsp").readlines() if x.split()[0].isdigit()]
+from heapq import *
 
-for line in lines:
-    x, y = line.split()
-    print "{},{}".format(float(x),float(y))
+def main():
+
+    lines = [" ".join(x.strip().split()[1:])
+             for x in open("files/rl11849.tsp").readlines()
+             if x.split()[0].isdigit()]
+
+    x_heap = []
+    y_heap = []
+    for num, line in enumerate(lines):
+        x, y = line.split()
+        heappush(x_heap, (float(x), num))
+        heappush(y_heap, (float(y), num))
+
+    print heapsort(x_heap)
+
+def heapsort(heap):
+    return [heappop(heap) for _ in range(len(heap))]
+
+if __name__ == "__main__":
+    main()
